cea6a40618e1f1f7ba2145b7812769e836a0e60b
shojol021 <shojol021@gmail.com>
2023-03-03 01:24:48 +0600

    Updated card properly with all the data from API

--- assignment6.js
+++ assignment6.js
@@ -6,33 +6,47 @@ const fetchData = async () => {
 
 }
 
+// ================================= Load Data ======================================
 const loadData = (tools) => {
     console.log(tools);
     const toolsContainer = document.getElementById('tools-container');
-    console.log(toolsContainer);
 
+    // tools = tools.slice(0, 6);
+
+    // =============== loop ==================
     tools.forEach(element => {
-        console.log(element);
+        // console.log(element);
         const div = document.createElement('div');
         div.classList.add('card', 'w-11/12', 'bg-base-100', 'shadow-xl');
         div.innerHTML = `
-            < figure > <img src=${element.image} alt="Shoes" /></figure>
+            <figure> <img src=${element.image} alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">
                     Features!
                     <div class="badge badge-secondary">NEW</div>
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <div class="badge badge-outline">Fashion</div>
-                    <div class="badge badge-outline">Products</div>
-                </div>
+                    <ol class="list-decimal list-inside">
+                        ${element.features.map(feature => `<li>${feature}</li>`).join("")}
+                    </ol>
+                    <hr>  
+                    <div class="flex flex-row justify-between">
+                        <div>
+                            <h1 class="font-bold text-2xl">${element.name}</h1>
+                            <h1>${element.published_in}</h1>
+                        </div>
+                        <div class="card-actions my-auto">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
             </div>
         `;
         toolsContainer.appendChild(div);
 
     });
+    // =============== loop End ================
 
 }
+
+// ================================= Load Data End ======================================
 
 fetchData();
