4df813a0b67dc7109debc28bce1b59aa14ed67a0
shojol021 <shojol021@gmail.com>
2023-03-03 03:39:04 +0600

    conditionally  date sort, added call funtion to make the code more friendly

--- assignment6.js
+++ assignment6.js
@@ -6,61 +6,8 @@ const fetchData = async () => {
 
 }
 
-// ================================= Load Data ======================================
-const loadData = (tools) => {
-    console.log(tools)
-
-    const toolsContainer = document.getElementById('tools-container');
-
-    // tools = tools.slice(0, 6);
-
-    // =============== loop ==================
-
-    tools.forEach(element => {
-        // console.log(element);
-        const div = document.createElement('div');
-        div.classList.add('card', 'w-11/12', 'bg-base-100', 'shadow-xl');
-        div.innerHTML = `
-            <figure> <img src=${element.image} alt="Shoes" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">
-                    Features!
-                    <div class="badge badge-secondary">NEW</div>
-                </h2>
-                    <ol class="list-decimal list-inside">
-                        ${element.features.map(feature => `<li>${feature}</li>`).join("")}
-                    </ol>
-                    <hr>  
-                    <div class="flex flex-row justify-between">
-                        <div>
-                            <h1 class="font-bold text-2xl">${element.name}</h1>
-                            <h1>${element.published_in}</h1>
-                        </div>
-                        <div class="card-actions my-auto">
-                            <i id=${element.id} class="fa-solid fa-arrow-right"></i>
-                        </div>
-                    </div>
-            </div>
-        `;
-        toolsContainer.appendChild(div);
-
-    });
-    // =============== loop End ================
-
-}
-// ================================= Load Data End ======================================
-
-
-const fetchData2 = async () => {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    const res = await fetch(url);
-    const info = await res.json();
-    loadData2(info.data.tools);
-
-}
-
 // ================================= Load Data 2 ======================================
-const loadData2 = (tools) => {
+const loadData = (tools, isSorted = false) => {
 
     document.getElementById('tools-container').innerHTML = '';
 
@@ -77,7 +24,7 @@ const loadData2 = (tools) => {
     // tools = tools.slice(0, 6);
 
     // =============== loop ==================
-    sortedArray.forEach(element => {
+    tools.forEach(element => {
         
         const div = document.createElement('div');
         div.classList.add('card', 'w-11/12', 'bg-base-100', 'shadow-xl');
@@ -113,8 +60,11 @@ const loadData2 = (tools) => {
 // ================================= Load Data 2 End ======================================
 
 
-document.getElementById('btn-sort').addEventListener('click', function(){
-    fetchData2();
+document.getElementById('btn-sort').addEventListener('click', async function(){
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const info = await res.json();
+    loadData(info.data.tools, isSorted = true);
 })
 
 fetchData();
