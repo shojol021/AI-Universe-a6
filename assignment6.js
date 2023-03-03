78262ef63cc72fe9eb585682553fa8bec810bde0
shojol021 <shojol021@gmail.com>
2023-03-04 01:49:59 +0600

    price information intigrations part updated dynamically perfection with ternary operatior

--- assignment6.js
+++ assignment6.js
@@ -14,29 +14,29 @@ const loadData = (tools, isSorted = false, limit = false,) => {
 
     document.getElementById('tools-container').innerHTML = '';
 
-    if(showAllClicked && !limit){
+    if (showAllClicked && !limit) {
         limit = false;
     } else {
         limit = 6;
     }
 
-    if(limit){
+    if (limit) {
         tools = tools.slice(0, 6);
     }
     // ========== Sorting array by date ===========
-    if (isSorted){
+    if (isSorted) {
         tools = tools.sort((a, b) => {
             const dateA = new Date(a.published_in);
             const dateB = new Date(b.published_in);
             return dateA - dateB;
-          });
+        });
     }
-    
+
     const toolsContainer = document.getElementById('tools-container');
 
     // =============== loop ==================
     tools.forEach(element => {
-        
+
         const div = document.createElement('div');
         div.classList.add('card', 'w-11/12', 'bg-base-100', 'shadow-xl');
         div.innerHTML = `
@@ -74,13 +74,13 @@ const loadData = (tools, isSorted = false, limit = false,) => {
 // ================================= Load Data End ======================================
 
 
-document.getElementById('btn-show-all').addEventListener('click', function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     showAllClicked = true;
     this.classList.add('hidden');
     fetchData()
 })
 
-document.getElementById('btn-sort').addEventListener('click', async function(){
+document.getElementById('btn-sort').addEventListener('click', async function () {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const info = await res.json();
@@ -102,45 +102,64 @@ const loadSingleTool = (tool) => {
 
     document.getElementById('headline').innerText = tool.description;
     document.getElementById('img').src = tool.image_link[0];
-    document.getElementById('input').innerText = tool.input_output_examples? `${tool.input_output_examples[0].input}` : 'No Data';
-    document.getElementById('output').innerText = tool.input_output_examples? `${tool.input_output_examples[0].input}`: 'sample';
+    document.getElementById('input').innerText = tool.input_output_examples ? `${tool.input_output_examples[0].input}` : 'Can you give any example?';
+    document.getElementById('output').innerText = tool.input_output_examples ? `${tool.input_output_examples[0].output}` : 'No, not yet! Take a break!!!';
     const features = document.getElementsByClassName('features');
-    const integrations = document.getElementsByClassName('integrations');
     const prices = document.getElementsByClassName('prices');
     const plans = document.getElementsByClassName('plans');
-    let i=1;
-    for (const feature of features){
+    let i = 1;
+    for (const feature of features) {
         feature.innerText = tool.features[i].feature_name;
         i++;
     }
-    let j=0;
-    for (const integration of integrations){
-        integration.innerText = tool.use_cases? `${tool.use_cases[j].name}`: 'no use case';
-        j++;
+    // let j=0;
+
+    const integrations = document.getElementById('integrations');
+    if (tool.use_cases === null) {
+        document.getElementById('integrations').innerText = '';
+        const li = document.createElement('li')
+        li.innerText = 'No Data Found';
+        integrations.appendChild(li);
+        
     }
 
-    let k = 0;
-    for (const price of prices){
-        if(tool.pricing === null){
-            price.innerText = 'No Price Information';
+    else {
+        document.getElementById('integrations').innerText = '';
+        for (const use_case of tool.use_cases) {
+            const li = document.createElement('li')
+            li.innerText = use_case.name;
+            integrations.appendChild(li);
         }
-        else{
-            price.innerText = tool.pricing[k].price == 'No cost' || tool.pricing[k].price == '0'? 'Free of cost' : `${tool.pricing[k].price}`;
+    }
+        // }
+        // else{
+        //     integration.innerText = tool.use_cases[j]? `${tool.use_cases[j].name}`: 'no use case';
+        // }
+
+
+        let k = 0;
+        console.log(tool.pricing)
+        for (const price of prices) {
+            if (tool.pricing === null) {
+                price.innerText = 'No Price Information';
+            }
+            else {
+                price.innerText = tool.pricing[k].price == 'No cost' || tool.pricing[k].price == '0' ? 'Free of cost' : `${tool.pricing[k].price}`;
+            }
+            k++;
         }
-        k++;
+
+        let l = 0;
+        for (const plan of plans) {
+            if (tool.pricing === null) {
+                plan.innerText = 'No package found'
+            }
+            else {
+                plan.innerText = tool.pricing[l].plan;
+            }
+            l++;
+        }
+
     }
 
-    let l = 0;
-    for (const plan of plans){
-        if(tool.pricing === null){
-            plan.innerText = 'No package found'
-        }
-        else{
-            plan.innerText = tool.pricing[l].plan;
-        }
-        l++;
-    }
-
-}
-
-fetchData(6);
+    fetchData(6);
